b9bd2b23c5d5bba369033e19ff21365072af4318
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-08-28 22:52:44 +0200

    add Content repo using Jdbc template

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,16 +3,16 @@ CREATE TABLE IF NOT EXISTS Content
     id           INTEGER AUTO_INCREMENT,
     title        VARCHAR(255) NOT NULL,
     desc         TEXT,
-    status       VARCHAR(20)  NOT NULL,
-    contentTpe   VARCHAR(50)  NOT NULL,
     url          VARCHAR(255),
+    status       VARCHAR(20)  NOT NULL,
+    content_type   VARCHAR(50)  NOT NULL,
     date_created TIMESTAMP    NOT NULL,
     date_updated TIMESTAMP,
 
     PRIMARY KEY (id)
 );
 
-INSERT INTO Content(title, desc, status, contentTpe, date_created)
+INSERT INTO Content(title, desc, status, content_type, date_created)
 VALUES ('My Spring Data Blog Post',
         'A Post about spring data',
         'IDEA',
