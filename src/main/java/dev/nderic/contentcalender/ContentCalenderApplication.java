c051163dfe962186eb135daed2c6ecb52f31f9de
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-08-28 12:35:14 +0200

    list core application beans

--- src/main/java/dev/nderic/contentcalender/ContentCalenderApplication.java
+++ src/main/java/dev/nderic/contentcalender/ContentCalenderApplication.java
@@ -2,12 +2,16 @@ package dev.nderic.contentcalender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ContentCalenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalenderApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ContentCalenderApplication.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
