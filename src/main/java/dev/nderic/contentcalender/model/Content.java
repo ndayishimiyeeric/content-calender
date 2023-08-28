c5f1c1485643a5b4710515a99c70aebb8d64ea9b
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-08-28 18:25:54 +0200

    CRUD operations for in memory data

--- src/main/java/dev/nderic/contentcalender/model/Content.java
+++ src/main/java/dev/nderic/contentcalender/model/Content.java
@@ -1,9 +1,12 @@
 package dev.nderic.contentcalender.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record Content(
         Integer id,
+        @NotBlank // Validate content title
         String title,
         String desc,
         String url,
--- src/main/java/dev/nderic/contentcalender/repository/ContentCollectionRepository.java
+++ src/main/java/dev/nderic/contentcalender/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package dev.nderic.contentcalender.repository;
 
 import dev.nderic.contentcalender.model.Content;
+import dev.nderic.contentcalender.model.Status;
+import dev.nderic.contentcalender.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +15,8 @@ import java.util.Optional;
 public class ContentCollectionRepository {
     private final List<Content> contents = new ArrayList<>();
 
-    ContentCollectionRepository() {}
+    public ContentCollectionRepository() {
+    }
 
     public List<Content> findAll() { // get all available contents
         return contents;
@@ -19,5 +24,34 @@ public class ContentCollectionRepository {
 
     public Optional<Content> findById(Integer id) {
         return contents.stream().filter(content -> content.id().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    // a method that needs to be executed after dependency injection is done to perform any initialization
+    private void init() {
+        Content content = new Content(
+                1,
+                "My First Blog post",
+                "First blog post",
+                null,
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null
+        );
+        contents.add(content);
+    }
+
+    public void save(Content content) {
+        contents.removeIf(prev -> prev.id().equals(content.id()));
+        contents.add(content);
+    }
+
+    public boolean existById(Integer id) {
+        return contents.stream().filter(content -> content.id().equals(id)).count() == 1;
+    }
+
+    public void deleteById(Integer id) {
+        contents.removeIf(prev -> prev.id().equals(id));
     }
 }
