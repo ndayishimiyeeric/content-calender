74a6d7bfe21cafce39ffdc293b03bd5edbda4134
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-08-28 13:02:28 +0200

    Get a bean signature by name

--- src/main/java/dev/nderic/contentcalender/ContentCalenderApplication.java
+++ src/main/java/dev/nderic/contentcalender/ContentCalenderApplication.java
@@ -2,12 +2,16 @@ package dev.nderic.contentcalender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ContentCalenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalenderApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ContentCalenderApplication.class, args);
+		RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate"); // getting our created RestTemplate
+		System.out.println(restTemplate); // print the signature
 	}
 
 }
