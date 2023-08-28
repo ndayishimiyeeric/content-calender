0f8c1b9c4db9afdc3887869bf96bb390dc6bf0ad
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-08-28 12:42:33 +0200

    adding a class to a bean factory using @Component annotation

---
+++ src/main/java/dev/nderic/contentcalender/Message.java
@@ -0,0 +1,10 @@
+package dev.nderic.contentcalender;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+    public String getMessage() {
+        return "Hello world";
+    }
+}
